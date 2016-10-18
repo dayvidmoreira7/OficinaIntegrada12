8084d47581171ef9dc562ce7f668db279dec1a9a
Dayvid Moreira <dayvid_tjk@hotmail.com>
2016-10-18 16:56:05 -0200

    Celular
    Incrementado

---
+++ Assets/GuardarPegar.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardarPegar : MonoBehaviour {
+
+	bool usando = true;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Tab) && !usando)
+		{
+			usando = true;
+		}
+		else if(Input.GetKeyDown(KeyCode.Tab) && usando)
+		{
+			usando = false;
+		}
+
+		gameObject.GetComponent<Animator> ().SetBool ("Pegar", usando);
+	}
+}
